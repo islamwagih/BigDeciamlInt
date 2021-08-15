113ac970e603556100f2674163df2b85a12c8606
Islam Wagih <islamwagihe@gmail.com>
2021-08-15 20:07:03 +0200

    Version 1

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include "BigDecimalInt.h"
+using namespace std;
+
+
+int main(){
+
+    BigDecimalInt a = "-5629595959";
+    BigDecimalInt b = "548894544655655555555555555555555555555555555555555555555555555555555555555555555555555555555555";
+    cout<<a-b<<endl;
+    cout<<b+a<<endl;
+    cout<<b-a<<endl;
+    return 0;
+}
